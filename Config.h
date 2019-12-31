5d50cf24bdf3c7293382b6954d83a676aac29bae
Nandy Bâ <nandyba@yahoo.fr>
2019-12-31 04:24:04 +0100

    Add config file comments

--- Config.h
+++ Config.h
@@ -3,5 +3,5 @@ char* MyApiKey = ""; // TODO: Change to your sinric API Key. Your API Key is dis
 char* MySSID = ""; // TODO: Change to your Wifi network SSID
 char* MyWifiPassword = ""; // TODO: Change to your Wifi network password
 
-String FirstDeviceID = "";
-String SecondDeviceID = "";
+String FirstDeviceID = ""; // TODO: Change to your sinric API first Device ID
+String SecondDeviceID = ""; // TODO: Change to your sinric API second Device ID
