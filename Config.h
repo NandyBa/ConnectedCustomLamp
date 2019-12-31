bdfe9104545785a710ad24ee0db03a792165e81e
Nandy Bâ <nandyba@yahoo.fr>
2019-12-31 04:04:11 +0100

    Add of the config file

---
+++ Config.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+char* MyApiKey = ""; // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+char* MySSID = ""; // TODO: Change to your Wifi network SSID
+char* MyWifiPassword = ""; // TODO: Change to your Wifi network password
+
+String FirstDeviceID = "";
+String SecondDeviceID = "";
